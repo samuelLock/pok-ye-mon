5200268a8bbbc9ed91306b38dd151b25b7e32cd0
samuello <samuel.t.lock@gmail.com>
2020-04-09 11:51:09 +0100

    Update app.py with relevent try catches

--- app.py
+++ app.py
@@ -6,17 +6,18 @@ app = Flask(__name__)
 baseUrl = "https://pokeapi.co/api/v2/pokemon-species/"
 shakespeareUrl = "https://api.funtranslations.com/translate/shakespeare"
 
-def extractDescription(pokemonData):
-    allDescriptions = pokemonData["flavor_text_entries"] # Try catch
-    description = extractEnglishDescription(allDescriptions)
+def extractDescription(pokemonData, name):
+    allDescriptions = pokemonData["flavor_text_entries"]
+    description = extractEnglishDescription(allDescriptions, name)
     return description
 
-def extractEnglishDescription(pokemonDescriptions):
+def extractEnglishDescription(pokemonDescriptions, name):
     engDescription = []
     for description in pokemonDescriptions:
-        if description["language"]["name"] == "en" and description["version"]["name"] == "y":
+        if description["language"]["name"] == "en" and description["version"]["name"] == "yellow":
             engDescription.append(description["flavor_text"])
-    # Check count is 1, if not throw error
+    if engDescription.count > 1:
+        raise IndexError("Expected 1 count of english descriptions for " + name + " in Pokemon Yellow. Instead " + str(engDescription.count) +" were found.")
     return engDescription[0]
 
 def replaceUnwantedCharacters(string :str, replacements :tuple) -> str:
@@ -25,17 +26,42 @@ def replaceUnwantedCharacters(string :str, replacements :tuple) -> str:
     return string
 
 def translateDescription(phrase: str) -> str:
-    r = requests.post(url = shakespeareUrl, data = {'text':phrase}) # Try catch
-    data = r.json()
-    translation = data["contents"]["translated"]
+    try:
+        r = requests.post(url = shakespeareUrl, data = {'text':phrase}) # Should never really throw an exception in theory, hence throwing 500.
+        data = r.json()
+    except:
+        return("Internal server error", 500)
+    
+    if r.status_code != 200:
+        return ("Bad Gateway (Shakespeare API)",502)
+
+    try:
+        translation = data["contents"]["translated"]
+    except:
+        return ("Bad Gateway (Shakespeare API)",502)
+        
     return translation
 
 @app.route('/pokemon/<name>')
 def name(name):
     url = baseUrl+name
-    r = requests.get(url = url) # Try catch
-    data = r.json()
-    description = extractDescription(data)
+    try:
+        r = requests.get(url = url) # Should never really throw an exception in theory, hence throwing 500.
+        data = r.json()
+    except:
+        return("Internal server error", 500)
+
+    if: r.status_code != 200: # Likely if the pokemon doesn't exist.
+        return("Not Found", 404)
+
+    try:
+        description = extractDescription(data, name)
+    except:
+        return ("Bad Gateway (Pokemon API)",502)
+    
+    if description == []:
+        return ("Not Found (Are you sure your Pokemon is from the first generation?)", 404)
+
     description = replaceUnwantedCharacters(json.dumps(description), (("\\n"," "), ("\\u00e9","e")))
     translatedDescription = translateDescription(description)
     return translatedDescription
