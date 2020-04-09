e3be1d5c87b310221d210baf2e3d668c6a662d5d
samuello <samuel.t.lock@gmail.com>
2020-04-09 16:28:03 +0100

    Added unit tests

--- unittests.py
+++ unittests.py
@@ -1,27 +1,61 @@
 import app
 import unittest
+import json
+import pytest
 
 class ExtractDescriptionTests(unittest.TestCase):
 
     def testExtractDescription(self):
-        extractedDescription = extractDescription(data, name)
+        with open('./unittest-cases/mew-full.json') as json_file:
+            data = json.load(json_file)
+            json_file.close()
+        with open('./unittest-cases/mew-eng-yellow-desc.txt') as txt_file:
+            mew_desc = txt_file.read()
+            txt_file.close()
+            extractedDescription = app.extractDescription(data, 'mew')
+            self.assertEqual(extractedDescription, mew_desc)
 
 class ExtractEnglishDescriptionTests(unittest.TestCase):
 
-    def standardDescription(self):
+    def testStandardDescription(self):
+        with open('./unittest-cases/mew-flavor-text-entries.json') as json_file:
+            data = json.load(json_file)
+            json_file.close()
+        with open('./unittest-cases/mew-eng-yellow-desc.txt') as txt_file:
+            mew_desc = txt_file.read()
+            txt_file.close()
+            extractedDescription = app.extractEnglishDescription(data, 'mew')
+            self.assertEqual(extractedDescription, mew_desc)
 
-    def multipleEnglishDescriptions(self):
+    def testMultipleYellowDescriptions(self):
+        with open('./unittest-cases/mew-flavor-text-entires-multiple-yellow.json') as json_file:
+            data = json.load(json_file)
+            json_file.close()
+        with open('./unittest-cases/mew-eng-yellow-desc.txt') as txt_file:
+            mew_desc = txt_file.read()
+            txt_file.close()
+            extractedDescription = app.extractEnglishDescription(data, 'mew')
+            self.assertEqual(extractedDescription, mew_desc)
 
-    def multipleYellowDescriptions(self):
-
-    def noDescription(self):
+    def testMultipleYellowEnglishDescriptions(self):
+        with open('./unittest-cases/mew-flavor-text-entries-multiple-yellow-and-english.json') as json_file:
+            data = json.load(json_file)
+            json_file.close()
+            with self.assertRaises(IndexError):
+                app.extractEnglishDescription(data, 'mew')
 
 class ReplaceUnwantedCharactersTests(unittest.TestCase):
 
-    def standardReplacement(self):
+    def testStandardReplacement(self):
+        replacedString = app.replaceUnwantedCharacters('this is a test', (('t','s',),('p','q')))
+        self.assertEqual(replacedString,'shis is a sess')
 
-    def emptyStringReplacement(self):
+    def testEmptyStringReplacement(self):
+        replacedString = app.replaceUnwantedCharacters('this is a test', ())
+        self.assertEqual(replacedString,'this is a test')
 
-    def nothingToReplace(self):
+    def testNothingToReplace(self):
+        replacedString = app.replaceUnwantedCharacters('this is a test', (('r','z',),('p','q')))
+        self.assertEqual(replacedString,'this is a test')
 
 
