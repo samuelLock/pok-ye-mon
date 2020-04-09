82060b929a5c74c0339350091420a29dc084737d
samuello <samuel.t.lock@gmail.com>
2020-04-09 11:07:21 +0100

    update app.py to translate into shakespearean english

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 
 baseUrl = "https://pokeapi.co/api/v2/pokemon-species/"
+shakespeareUrl = "https://api.funtranslations.com/translate/shakespeare"
 
 def extractDescription(pokemonData):
-    allDescriptions = pokemonData["flavor_text_entries"]
+    allDescriptions = pokemonData["flavor_text_entries"] # Try catch
     description = extractEnglishDescription(allDescriptions)
     return description
 
@@ -15,12 +16,26 @@ def extractEnglishDescription(pokemonDescriptions):
     for description in pokemonDescriptions:
         if description["language"]["name"] == "en" and description["version"]["name"] == "y":
             engDescription.append(description["flavor_text"])
+    # Check count is 1, if not throw error
     return engDescription[0]
+
+def replaceUnwantedCharacters(string :str, replacements :tuple) -> str:
+    for pair in replacements:
+        string = string.replace(pair[0], pair[1])
+    return string
+
+def translateDescription(phrase: str) -> str:
+    r = requests.post(url = shakespeareUrl, data = {'text':phrase}) # Try catch
+    data = r.json()
+    translation = data["contents"]["translated"]
+    return translation
 
 @app.route('/pokemon/<name>')
 def name(name):
     url = baseUrl+name
-    r = requests.get(url = url)
+    r = requests.get(url = url) # Try catch
     data = r.json()
     description = extractDescription(data)
-    return json.dumps(description)
+    description = replaceUnwantedCharacters(json.dumps(description), (("\\n"," "), ("\\u00e9","e")))
+    translatedDescription = translateDescription(description)
+    return translatedDescription
